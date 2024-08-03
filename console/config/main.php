a50b9eb662b4ee5d471c2993cc1f8393f3803c2e
Werner Garcia <werner.garcia@impartner.com>
2024-08-03 04:08:50 -0600

    Small fix

--- console/config/main.php
+++ console/config/main.php
@@ -1,5 +1,8 @@
 <?php
 
+use yii\console\controllers\FixtureController;
+use yii\log\FileTarget;
+
 $params = array_merge(
     require __DIR__ . '/../../common/config/params.php',
     require __DIR__ . '/../../common/config/params-local.php',
@@ -18,7 +21,7 @@ return [
     ],
     'controllerMap' => [
         'fixture' => [
-            'class' => \yii\console\controllers\FixtureController::class,
+            'class' => FixtureController::class,
             'namespace' => 'common\fixtures',
           ],
     ],
@@ -26,7 +29,7 @@ return [
         'log' => [
             'targets' => [
                 [
-                    'class' => \yii\log\FileTarget::class,
+                    'class' => FileTarget::class,
                     'levels' => ['error', 'warning'],
                 ],
             ],
